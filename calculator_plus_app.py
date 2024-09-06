28c4d905300a9c6e24abcfd8db6e26d66a7f1663
Ferocious71 <salmanmuneeb@gmail.com>
2024-09-06 23:11:46 +0530

    Square Root function has been added

---
+++ calculator_plus_app.py
@@ -0,0 +1,9 @@
+# Implement the following function to calculate the square root of a number.
+import math
+
+def square_root(self, x):
+ return math.sqrt(x)
+
+
+num3 = 25
+print(f"The square root of {num3} = {calculator.square_root(num3)}")
